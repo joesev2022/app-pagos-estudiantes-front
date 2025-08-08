08d1d41ebe1ffa03651614d2072d730b94115106
Jose_Apaza <jose.apazasev@gmail.com>
2025-08-08 12:16:50 -0500

    added responsive effect on ToolBar Header

--- src/app/admin-template/admin-template.ts
+++ src/app/admin-template/admin-template.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Auth } from '../services/auth';
+import {BreakpointObserver, Breakpoints, LayoutModule} from '@angular/cdk/layout';
+import { map, Observable, shareReplay } from 'rxjs';
 
 @Component({
   selector: 'app-admin-template',
@@ -9,7 +11,16 @@ import { Auth } from '../services/auth';
 })
 export class AdminTemplate {
 
-  constructor(public authService:Auth) { }
+   isSmall$!: Observable<boolean>;
+
+  constructor(public authService:Auth, private bp: BreakpointObserver) {
+    this.isSmall$ = this.bp
+      .observe([Breakpoints.Handset, Breakpoints.TabletPortrait])
+      .pipe(
+        map(r => r.matches),
+        shareReplay(1)
+      );
+  }
 
   logout():void {
     this.authService.logout();
